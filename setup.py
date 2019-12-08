9a928b6fa340033947dcbf7c7f02b9f5e90c3e48
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-07 23:42:58 -0500

    Added setup.py in preparation for package development

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+        name="UMREDCap",
+        version="0.0.1",
+        author="Kevin Davis",
+        author_email="kevin.davis@med.miami.edu",
+        description=("Package of classes and functions for UM DOCS MedIT"
+                     "Specific functions related to REDCap"),
+        license="BSD",
+        keywords="UM DOCS MedIT REDCap",
+        url="http://github.com/umdocsmedit/umredcap",
+        packages=['umredcap', 'tests'],
+        long_description=read('README.md'),
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Education',
+            'Programming Language :: Python',
+            'Topic :: Utilities'
+            ]
+        )
