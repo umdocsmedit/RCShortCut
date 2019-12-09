66bc91671b7213dca586b0dfb52987264f2db5a3
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-09 12:46:59 -0500

    Expose the modules

--- umredcap/__init__.py
+++ umredcap/__init__.py
@@ -1,2 +1,13 @@
+import umredcap.creds as creds
+import umredcap.events as events
+import umredcap.records as records
+
+# use variables so flake8 doesn't complate
+creds
+events
+records
+
+
+# initial main
 def main() -> int:
     return 0
