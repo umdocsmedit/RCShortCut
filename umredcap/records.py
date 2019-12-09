7dcc4ed999eac81f3cce11a7a669c6045113a288
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-09 12:46:22 -0500

    Adding some event records requests

---
+++ umredcap/records.py
@@ -0,0 +1,55 @@
+import math
+import logging
+import requests
+
+import pandas as pd  # type: ignore
+import umredcap.api as api
+
+from typing import List
+
+
+LOGGER = logging.getLogger('records')
+
+
+def export_event_records(
+        project='new_redcap',
+        events: List[str] = []
+        ) -> pd.DataFrame:
+
+    data = pd.DataFrame()
+
+    request_data = {
+            'project': project,
+            'content': 'record',
+            'format': 'json',
+            'type': 'flat',
+            'rawOrLabel': 'raw',
+            'rawOrLabelHeaders': 'raw',
+            'exportCheckboxLabel': 'false',
+            'exportSurveyFields': 'false',
+            'exportDataAcessGroups': 'false',
+            'returnFormat': 'json'
+            }
+
+    n_events_per_request: int = 5
+    n_requests = math.ceil(len(events) / n_events_per_request)
+
+    for request_set in range(n_requests):
+        LOGGER.debug(f'Processing request {request_set} of {n_requests-1}')
+
+        first_event_index: int = request_set * n_events_per_request
+        last_event_index: int = first_event_index + n_events_per_request
+        request_events: List[str] = events[first_event_index:last_event_index]
+
+        for index, event in enumerate(request_events):
+            key = f'events[{index}]'
+            value = event
+            request_data[key] = value
+
+        response: requests.Response = api.request(request_data)
+        response_data: pd.DataFrame = pd.read_json(
+                response.text, convert_dates=False
+                )
+        data = data.append(response_data, ignore_index=True)
+
+    return data
