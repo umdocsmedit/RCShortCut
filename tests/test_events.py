7810b7ebb38c8f28a76a638bb3418dc715b808e4
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-13 00:26:18 -0500

    update to tests to focus on changes

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,39 +1,55 @@
+import pytest  # type: ignore
 import pandas as pd  # type: ignore
+import umredcap.creds as creds
 import umredcap.events as events
+
+from umredcap.project import Project
 
 from typing import List
 
 
-def test_export_arms():
+@pytest.fixture
+def handlers() -> events.arm_handler:
+    credentials: creds.creds = creds.creds()
+    ah: events.arm_handler = events.arm_handler(credentials)
 
-    arms: pd.DataFrame = events.export_arms()
+    return ah
+
+
+def test_export_arms(handlers):
+    ah = handlers
+
+    arms: pd.DataFrame = ah.export_arms()
 
     assert arms.shape[0] != 0
 
 
-def test__get_num_arms():
+def test__get_num_arms(handlers):
+    ah = handlers
 
-    n_arms: int = events._get_num_arms()
+    n_arms: int = ah._get_num_arms()
     assert n_arms > 0
 
 
-def test__generate_arms_list():
+def test__generate_arms_list(handlers):
+    ah = handlers
 
     # if blank it should produce an range array from 1 to max arms
-    max_arms: int = events._get_num_arms()
-    arms_list: List[int] = events._generate_arms_list()
+    max_arms: int = ah._get_num_arms()
+    arms_list: List[int] = ah._generate_arms_list()
 
     assert max(arms_list) == max_arms
 
     # Should simply return the list it is given otherwise
     input: List[int] = [4, 5, 6, 7]
-    output: List[int] = events._generate_arms_list(input)
+    output: List[int] = ah._generate_arms_list(input)
 
     assert input == output
 
 
-def test_get_event_names():
+def test_get_event_names(handlers):
 
+    main_project: Project = Project()
     expected: List[str] = [
             '20172018_arm_4',
             '20172018_arm_5',
@@ -42,7 +58,7 @@ def test_get_event_names():
             ]
     arms: List[int] = [4, 5]
     prev_years = 2
-    output: List[str] = events.get_event_names(
+    output: List[str] = main_project.get_event_names(
             arms,
             previous_n_years=prev_years
             )
