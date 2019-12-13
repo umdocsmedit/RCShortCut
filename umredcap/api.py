7358f23a2fcbea6df913b40784188246b8793767
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-13 00:24:49 -0500

    Move api functions to a class

--- umredcap/api.py
+++ umredcap/api.py
@@ -1,19 +1,24 @@
 import requests
-import umredcap.creds as creds
+
+from umredcap.creds import creds
 
 
-def resolve_project(options: dict) -> dict:
-    if 'project' not in options:
+class api:
+
+    def __init__(self, my_creds: creds):
+        self.creds = my_creds
+
+    def resolve_project(self, options: dict) -> dict:
+        if 'project' not in options:
+            return options
+
+        project_name = options['project']
+        credentials = self.creds.load()
+        options['token'] = credentials[project_name]
+        del options['project']
+
         return options
 
-    project_name = options['project']
-    credentials = creds.load()
-    options['token'] = credentials[project_name]
-    del options['project']
-
-    return options
-
-
-def request(options: dict) -> requests.Response:
-    options = resolve_project(options)
-    return requests.post('https://redcap.miami.edu/api/', options)
+    def request(self, options: dict) -> requests.Response:
+        options = self.resolve_project(options)
+        return requests.post('https://redcap.miami.edu/api/', options)
