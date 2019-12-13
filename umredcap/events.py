a7d11411c8e4f86bfd533a4575daf890bcf8f07a
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-13 00:24:27 -0500

    remove event_handler

--- umredcap/events.py
+++ umredcap/events.py
@@ -1,45 +1,38 @@
 import time
 
-import umredcap.api as api
 import requests
 import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
 
+from umredcap.api import api
+from umredcap.creds import creds
 from typing import List
 
 
-def export_arms(project='new_redcap') -> pd.DataFrame:
-    data = {
-            'project': project,
-            'content': 'arm',
-            'format': 'json',
-            'returnFormat': 'json'
-            }
+class arm_handler:
 
-    response: requests.Response = api.request(data)
-    return pd.read_json(response.text)
+    def __init__(self, my_creds: creds):
+        self.api = api(my_creds)
 
+    def export_arms(self, project='new_redcap') -> pd.DataFrame:
+        data = {
+                'project': project,
+                'content': 'arm',
+                'format': 'json',
+                'returnFormat': 'json'
+                }
 
-def export_events(project='new_redcap') -> pd.DataFrame:
-    data = {
-            'project': project,
-            'content': 'event',
-            'format': 'json',
-            'returnFormat': 'json'
-            }
-    response: requests.Response = api.request(data)
-    return pd.read_json(response.text)
+        response: requests.Response = self.api.request(data)
+        return pd.read_json(response.text)
 
+    def _get_num_arms(self) -> int:
+        arms_df = self.export_arms()
+        return arms_df.shape[0]
 
-def _get_num_arms() -> int:
-    arms_df = export_arms()
-    return arms_df.shape[0]
-
-
-def _generate_arms_list(arms: List[int] = []) -> List[int]:
-    total_arms = _get_num_arms()
-    arms = arms if len(arms) > 0 else np.arange(0, total_arms)+1
-    return arms
+    def _generate_arms_list(self, arms: List[int] = []) -> List[int]:
+        total_arms = self._get_num_arms()
+        arms = arms if len(arms) > 0 else np.arange(0, total_arms)+1
+        return arms
 
 
 def _get_cur_academic_years() -> tuple:
@@ -84,25 +77,3 @@ def _generate_years_list(years: List[int], previous_n_years: int) -> List[str]:
     years_list = list(set(years_list))
     years_list.sort()
     return years_list
-
-
-def get_event_names(
-        arms: List[int] = [],
-        years: List[int] = [],
-        previous_n_years: int = 0) -> List[str]:
-    """Returns a list of event names given the arm numbers and either an array
-    of academica years (beginning in the fall) or an integer of the previous n
-    number of years. If both years, and previous_n_years are used, they are
-    merged and uniqued"""
-
-    year_strs = _generate_years_list(years, previous_n_years)
-    events = []
-    for year_str in year_strs:
-        for arm in arms:
-            event = f'{year_str}_arm_{arm}'
-            events.append(event)
-
-    events_df: pd.DataFrame = export_events()
-    event_names: List[str] = events_df['unique_event_name'].to_list()
-    events = [x for x in events if x in event_names]
-    return events
