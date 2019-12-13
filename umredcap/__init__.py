b2069164583271f2a858f32cff34edfd40eb9d69
Kevin Davis <kevincarrolldavis@gmail.com>
2019-12-13 00:25:51 -0500

    Export project

--- umredcap/__init__.py
+++ umredcap/__init__.py
@@ -2,10 +2,13 @@ import umredcap.creds as creds
 import umredcap.events as events
 import umredcap.records as records
 
+from umredcap.project import Project
+
 # use variables so flake8 doesn't complate
 creds
 events
 records
+Project
 
 
 # initial main
